7832a028d29a41a4c8a56383e3094c6c6d7ab115
Pierluigi De Palo <pierluigidepalo@gmail.com>
2025-07-15 19:33:23 +0200

    Update open_library.py

--- fetchers/open_library.py
+++ fetchers/open_library.py
@@ -1,16 +1,16 @@
 import requests
 import pandas as pd
 
-def fetch_open_library(subject, top_n=5):
-    """Restituisce work_count e top_n subjects"""
-    key = subject.replace(' ', '_')
-    url = f"https://openlibrary.org/subjects/{key}.json"
+def fetch_openlib_subject(subject):
+    """
+    Recupera da OpenLibrary il numero di opere sotto il subject indicato.
+    """
+    key = subject.replace(" ", "_")
+    url = f"https://openlibrary.org/subjects/{key}.json?limit=1"
     res = requests.get(url, timeout=10)
     res.raise_for_status()
     data = res.json()
-    work_count = data.get('work_count', 0)
-    subjects = [s['name'] for s in data.get('subject', [])][:top_n]
     return pd.DataFrame([{
-        'work_count': work_count,
-        'top_subjects': ','.join(subjects)
+        "subject": subject,
+        "work_count": data.get("work_count", 0)
     }])
